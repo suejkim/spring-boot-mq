2b5537da8464b733121ebaf6b6dd33cbb926c052
sjkim <abbccc0225@gmail.com>
2022-08-09 23:47:28 +0900

    ADD: new project for mq test

---
+++ src/test/java/com/sjkim/springbootmq/SpringBootMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjkim.springbootmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

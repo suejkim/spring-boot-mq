d49336fc4dc1bce6bcec5cb74aab9688f1a5444b
sjkim <abbccc0225@gmail.com>
2022-08-11 23:10:30 +0900

    UPDATE: build.gradle spring-boot-starter-amqp

--- mq/module-mq/src/main/java/com/sjkim/modulemq/ModuleMqApplication.java
+++ mq/module-mq/src/main/java/com/sjkim/modulemq/ModuleMqApplication.java
@@ -1,13 +1,8 @@
 package com.sjkim.modulemq;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ModuleMqApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ModuleMqApplication.class, args);
-    }
 
 }
